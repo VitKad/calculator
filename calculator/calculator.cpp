d299809ad8aada5aa24ff9387772ea07abb603ba
Viktor Kadyko <victor_-kadiko@mail.ru>
2020-12-23 23:33:44 +0300

    Добавлен скелет проекта. Ввод значений, а так же оператор множественного выбора

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -2,10 +2,59 @@
 //
 
 #include "stdafx.h"
-
-
-int _tmain(int argc, _TCHAR* argv[])
+using namespace std;
+int main()
 {
-	return 0;
-}
+	setlocale(LC_ALL, "Rus");
+	float a, b, c;
+	char m;
+	cout << "Выберите действие: ";
+	cout << "\n";
+	cout << "|           Меню            |\n";
+	cout << "|---------------------------|\n";
+	cout << "|  +  |       Сложение      |\n";
+	cout << "|  -  |       Разность      |\n";  
+	cout << "|  *  |      Умножение      |\n";
+	cout << "|  /  |       Деление       |\n";
+	cout << "|  &  |  Квадратный корень  |\n";
+	cout << "|  ^  | Возвести в степень  |\n";
+	cin >> m;
+	cout << "Введите первое число: ";
+	cin >> a;
+	if (m != '^' && m != '&')
+	{
+		cout << "Введите второе число: ";
+		cin >> b;
+	}
 
+	switch (m)
+	{
+		case '+':
+			{
+						break;
+			}
+		case '-':
+			{
+						break;
+			}
+		case '*':
+			{
+						break;
+			}
+		case '/':
+			{
+						break;
+			}
+		case '&':
+			{
+						break;
+			}
+		case '^':
+			{
+						break;
+			}
+		default:
+			cout << "Выбранной операции не существует";
+	}
+	system("PAUSE");
+}
